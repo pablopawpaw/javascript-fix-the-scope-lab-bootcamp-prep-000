bd5401036781b86a7224c546ee61cfb44794aa09
lpault29@gmail.com Ly <lpault29@gmail.com>
2018-04-16 21:20:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ function yourAnimal() {
 }
 console.log(yourAnimal());
 
-
-function add2(n) {
   const two = 2
+function add2(n) {
+
   return parseInt(n + two) 
 
   // Feel free to move things around!
